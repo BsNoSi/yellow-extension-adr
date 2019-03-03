c2036b0df6fae9f53138889bdc5a4ab9fdc0082d
BsNoSi <github@buoa.de>
2019-03-03 19:57:33 +0100

    Update for new Yellow API

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
-# Yellow Plugin Adr
+# Yellow Extention Adr
 
 Creator for well formated adress display
 
 ## The Idea Behind
 
-Sometimes you need a more or less big amount of adress data *well formated* on a web site. To ease this, I created this plugin.
+Sometimes you need a more or less big amount of different adress data *well formated* on a web site. To ease this, I created this extention for Yellow-CMS.
 
 ## How do I Install This?
 
 1. Download and install [Datenstrom Yellow CMS](https://github.com/datenstrom/yellow/).
-2. Download [adr plugin](https://github.com/BsNoSi/yellow-plugin-adr/archive/master.zip). If you are using Safari, right click and select 'Download file as'.
-3. Copy the `yellow-plugin-adr-master.zip` into the `system/plugins` folder.
+2. Download [adr plugin](https://github.com/BsNoSi/yellow-features/raw/master/zip/adr.zip). If you are using Safari, right click and select 'Download file as'.
+3. Copy the `adr-master.zip` into the `system/plugins` folder.
 
-To uninstall simply delete the [plugin files](https://github.com/BsNoSi/yellow-plugin-ontime/blob/master/update.ini).
+To uninstall simply delete the [extention files](https://github.com/BsNoSi/yellow-features/blob/master/adr/extention.ini).
 
-## Using the adr plugin
+## Using the adr extention
 
 `[adr name1 street city phone website e-mail-adress name2 fax mobile]`
 
-All given parts are displayed in a structured form, you can leave out what you want. The order follows (descending my) typical availability/need of information details.
+All given parts are displayed in a structured form. You can leave out what you want. The order follows (descending my) typical availability and need of information details.
 
-The output is in different order, only available (given) information is displayed and may look like this (see additional css below):
+The output is in different order, only available (given) information is displayed. To skip parts use a hyphen. A result may look like this:
 
 ![sample-display](sample-display.png)
 
@@ -31,50 +31,10 @@ There is no check if
 - the address has a meaningful display in google maps
 - any of the given addresses are valid
 
-### Translations
+### Additional Files
 
-The plugin contains English and German translations. You may add your language by inserting it into the php file:
-
-```php+HTML
-if (preg_match('/de/i', $lng)) {
-				$hdl = 'Kontaktdaten';
-				$hdlink = 'Externer Link zur Anbieterseite';
-				$hdmap = 'In GoogleMaps anzeigen';
-				$hdq = 'Anfrage%20von%20';
-				$hdem = 'E-Mail per Mailprogramm';
-				$hddis = 'Das Nutzen externer Links erfolgt auf eigene Gefahr.<br/>Sie sind <b>kein Bestandteil</b> dieser Webseite(n).';
-			} 
-```
-Here you can insert an 
-
-```php
-else
-if (preg_match('/yourlanguage/i', $lng)) {
-…
-}
-```
-
-copying the first part and changing the language and translations as required. Or you simply translate the German area to your required language and keep English as fallback.
-
-```php+HTML
-			else {
-				$hdl = 'Contact Details';
-				$hdlink = 'External link to provider page';
-				$hdmap = 'Show in GoogleMaps';
-				$hdq = 'Inquiry%20from%20';
-				$hdem = 'E-Mail by your E-Mail Program';
-				$hddis = 'The use of external links is at your own risk.<br/>They are <b>not part</b> of this website.';
-```
-
-## Additional css
-
-To get the utf-8 symbols *well aligned* and to highlight the disclaimer for external links you may add this to your style sheet and modify it depending to your needs:
-
-```css
-div.adr {border-radius: 0.2rem; border: 0.1rem solid #F2C11E; padding:0.3rem; margin:1rem 0;background-color:#f7f7f7;display:inline-block;font-size:0.8em;}
-span.adr {font-size: 1.1em; color:#094A7A;font-weight:bold;display:inline-block;min-width:1.8em;}
-```
-
+- Translation files for German, English and French are included. You may change the texts to you needs.
+- The css to achieve the displayed result above is included. You may change it to your theme style.
 
 
 ## Developer
--- adr.php
+++ adr.php
@@ -3,9 +3,11 @@
 // Copyright (c) 2018 Norbert Simon, nosi@nosi.de
 // This file may be used and distributed under the terms of the public license.
 // 2018-11-12 Initial Release
+// 2019-03-03 Update to new YELLOW API, language stripped from code to files
+// Icons gegen text getauscht
 
 class Yellowadr {
-    const VERSION = "1.0";
+    const VERSION = "1.1";
     public $yellow;         //access to API
     
     // Handle initialisation
@@ -18,36 +20,31 @@ class Yellowadr {
         if ($name=="adr" && $shortcut) {
 			list($who,$street, $city, $phone, $web, $mail,$person,$fax,$mobile) = $this->yellow->toolbox->getTextArgs($text);
 			$root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
-			$lng = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-			if (preg_match('/de/i', $lng)) {
-				$hdl = 'Kontaktdaten';
-				$hdlink = 'Externer Link zur Anbieterseite';
-				$hdmap = 'In GoogleMaps anzeigen';
-				$hdq = 'Anfrage%20von%20';
-				$hdem = 'E-Mail per Mailprogramm';
-				$hddis = 'Das Nutzen externer Links erfolgt auf eigene Gefahr.<br/>Sie sind <b>kein Bestandteil</b> dieser Webseite(n).';
-			} 
-			else {
-				$hdl = 'Contact Details';
-				$hdlink = 'External link to provider page';
-				$hdmap = 'Show in GoogleMaps';
-				$hdq = 'Inquiry%20from%20';
-				$hdem = 'E-Mail by your E-Mail Program';
-				$hddis = 'The use of external links is at your own risk.<br/>They are <b>not part</b> of this website.';
-			}
+			$lng = $this->yellow->page->get("language");
+						
             $result = preg_replace('/(\n|\s)+/i','+',$street . '+' . $city);
-			$output .='<div><h3>'.$hdl.'</h3>';
+			$output .='<div><h3>'.$this->yellow->text->getTextHtml("adr_contactdata", $lng ).'</h3>';
 			$output .= '<span class="adr">' . $who . '</span>';
 			if(!empty($person)) $output .= '<br/>'. $person;
-			$output .= '<br/><span class="adr">&#128393;</span>'.$street.'<br/><span class="adr">&nbsp;</span>'.$city;
-			$output .='<br/><span class="adr">&#9873;</span><a title="'.$hdmap.'" href="https://www.google.com/maps/place/' . $result .'" target="map">'.$hdmap.'</a>';
-			if(!empty($phone)) $output .= '<br/><span class="adr">&#8481;</span>' . $phone;
-			if(!empty($mobile)) $output .= '<br/><span class="adr">&#128385;</span>' . $mobile;
-			if(!empty($fax)) $output .= '<br/><span class="adr">&#8507;</span>' . $fax;
-			if(!empty($web)) $output .= '<br/><span class="adr">&#10150;</span><a href="https://' . $web . '" target="ex" title="'.$hdlink.'">' . $web . '</a>';
-			if(!empty($mail)) $output .= '<br/><span class="adr">&#9993;</span><a href="mailto://' . $mail . '?subject='.$hdq.$root.$page->location.'" title="'.$hdem.'">' . $mail . '</a>';
+			$output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Address", $lng ).'</span>'.$street.'<br/><span class="adr">&nbsp;</span>'.$city;
+			$output .='<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Map", $lng ).'</span><a title="'.$this->yellow->text->getTextHtml("adr_ShowMaps", $lng ).'" href="https://www.google.com/maps/place/' . $result .'" target="map">'.$this->yellow->text->getTextHtml("adr_ShowMaps", $lng ).'</a>';
+			if(!empty($phone)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Tel", $lng ).'</span>' . $phone;
+			if(!empty($mobile)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Mobile", $lng ).'</span>' . $mobile;
+			if(!empty($fax)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Fax", $lng ).'</span>' . $fax;
+			if(!empty($web)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Web", $lng ).'</span><a href="https://' . $web . '" target="ex" title="'.$this->yellow->text->getTextHtml("adr_ExternalLink", $lng ).'">' . $web . '</a>';
+			if(!empty($mail)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Mail", $lng ).'</span><a href="mailto://' . $mail . '?subject='.$this->yellow->text->getTextHtml("adr_InquiredBy", $lng ).$root.$page->location.'" title="'.$this->yellow->text->getTextHtml("adr_ByMail", $lng ).'">' . $mail . '</a>';
 			$output .= '</div>';
-			$output .= '<div class="adr">'.$hddis.'</div>';
+			$output .= '<div class="adr">'.$this->yellow->text->getText("adr_OwnRisk", $lng ).'</div>';
+        }
+        return $output;
+    }
+	
+	// Handle page extra data
+    public function onParsePageExtra($page, $name) {
+        $output = null;
+        if ($name=="header") {
+            $extensionLocation = $this->yellow->system->get("serverBase").$this->yellow->system->get("extensionLocation");
+            $output = "<link rel=\"stylesheet\" type=\"text/css\" media=\"all\" href=\"{$extensionLocation}adr.css\" />\n";
         }
         return $output;
     }
