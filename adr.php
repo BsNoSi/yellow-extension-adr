690230072f514e19460d7326216af84d0c9ea62d
BsNoSi <github@buoa.de>
2019-03-04 07:43:57 +0100

    Cleaned version

--- adr.php
+++ adr.php
@@ -20,21 +20,20 @@ class Yellowadr {
         if ($name=="adr" && $shortcut) {
 			list($who,$street, $city, $phone, $web, $mail,$person,$fax,$mobile) = $this->yellow->toolbox->getTextArgs($text);
 			$root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'];
-			$lng = $this->yellow->page->get("language");
-						
+									
             $result = preg_replace('/(\n|\s)+/i','+',$street . '+' . $city);
-			$output .='<div><h3>'.$this->yellow->text->getTextHtml("adr_contactdata", $lng ).'</h3>';
+			$output .='<div><h3>'.$this->yellow->text->getHtml("adr_contactdata").'</h3>';
 			$output .= '<span class="adr">' . $who . '</span>';
 			if(!empty($person)) $output .= '<br/>'. $person;
-			$output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Address", $lng ).'</span>'.$street.'<br/><span class="adr">&nbsp;</span>'.$city;
-			$output .='<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Map", $lng ).'</span><a title="'.$this->yellow->text->getTextHtml("adr_ShowMaps", $lng ).'" href="https://www.google.com/maps/place/' . $result .'" target="map">'.$this->yellow->text->getTextHtml("adr_ShowMaps", $lng ).'</a>';
-			if(!empty($phone)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Tel", $lng ).'</span>' . $phone;
-			if(!empty($mobile)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Mobile", $lng ).'</span>' . $mobile;
-			if(!empty($fax)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Fax", $lng ).'</span>' . $fax;
-			if(!empty($web)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Web", $lng ).'</span><a href="https://' . $web . '" target="ex" title="'.$this->yellow->text->getTextHtml("adr_ExternalLink", $lng ).'">' . $web . '</a>';
-			if(!empty($mail)) $output .= '<br/><span class="ico">'.$this->yellow->text->getTextHtml("adr_Mail", $lng ).'</span><a href="mailto://' . $mail . '?subject='.$this->yellow->text->getTextHtml("adr_InquiredBy", $lng ).$root.$page->location.'" title="'.$this->yellow->text->getTextHtml("adr_ByMail", $lng ).'">' . $mail . '</a>';
+			$output .= '<br/><span class="ico">'.$this->yellow->text->getHtml("adr_Address").'</span>'.$street.'<br/><span class="adr">&nbsp;</span>'.$city;
+			$output .='<br/><span class="ico">'.$this->yellow->text->getHtml("adr_Map").'</span><a title="'.$this->yellow->text->getHtml("adr_ShowMaps").'" href="https://www.google.com/maps/place/' . $result .'" target="map">'.$this->yellow->text->getHtml("adr_ShowMaps").'</a>';
+			if(!empty($phone)) $output .= '<br/><span class="ico">'.$this->yellow->text->getHtml("adr_Tel").'</span>' . $phone;
+			if(!empty($mobile)) $output .= '<br/><span class="ico">'.$this->yellow->text->getHtml("adr_Mobile").'</span>' . $mobile;
+			if(!empty($fax)) $output .= '<br/><span class="ico">'.$this->yellow->text->getHtml("adr_Fax").'</span>' . $fax;
+			if(!empty($web)) $output .= '<br/><span class="ico">'.$this->yellow->text->getHtml("adr_Web").'</span><a href="https://' . $web . '" target="ex" title="'.$this->yellow->text->getHtml("adr_ExternalLink").'">' . $web . '</a>';
+			if(!empty($mail)) $output .= '<br/><span class="ico">'.$this->yellow->text->getHtml("adr_Mail").'</span><a href="mailto://' . $mail . '?subject='.$this->yellow->text->getHtml("adr_InquiredBy").$root.$page->location.'" title="'.$this->yellow->text->getHtml("adr_ByMail").'">' . $mail . '</a>';
 			$output .= '</div>';
-			$output .= '<div class="adr">'.$this->yellow->text->getText("adr_OwnRisk", $lng ).'</div>';
+			$output .= '<div class="adr">'.$this->yellow->text->get("adr_OwnRisk").'</div>' . $lng;;
         }
         return $output;
     }
